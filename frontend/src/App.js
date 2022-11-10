97b14d30907fac13b2cb64e433adbd8d2838debc
ABHISHEK KUMAR <101967057+abhishekadityaroy102@users.noreply.github.com>
2022-11-10 22:23:54 +0530

    backend commplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import { Box, Text } from "@chakra-ui/react";
-import "./App.css";
-import { Navbar } from "./components/Navbar/Navbar";
 
+import "./App.css";
+import { Box} from "@chakra-ui/react";
+import "./App.css";
+
+// import { Navbar } from "./components/Navbar/Navbar";
+// import Footer from "./Pages/Footer";
+// import MenPage from "./Pages/MenPage";
+import { AllRoutes } from "./Pages/AllRoutes";
 function App() {
   return (
+    <>
     <Box className="font-face-pt ">
-      <Navbar />
-    </Box>
+     <AllRoutes />
+     </Box>
+    </>
   );
 }
 
--- frontend/src/components/Navbar/SearchInput.jsx
+++ frontend/src/components/Navbar/SearchInput.jsx
@@ -1,18 +1,39 @@
-import { Box, Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { Box, Button, Input, InputGroup, InputRightElement, useToast } from '@chakra-ui/react'
 import React from 'react'
 import {Search2Icon} from '@chakra-ui/icons';
+import { useDispatch } from 'react-redux';
+import { searchProduct } from '../../Redux/App/action';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 export const SearchInput = () => {
+  const [item,setItem]=useState("")
+  const Navigate=useNavigate()
+  const toast=useToast();
+  const dispatch=useDispatch();
     const HandleSearch=()=>{
-        alert("helo")
+      if(item===""){
+        toast({
+          title: 'Input is empty',
+          status: 'error',
+          duration: 5000,
+          position:"top",
+          isClosable: true,
+        })
+      }else{
+        dispatch(searchProduct(item))
+        Navigate("/searchproduct")
+      }
+      
     }
+    
   return (
     <Box ml={5} w="50%">
        
         <InputGroup size='md'>
-        <Input borderRadius={"25px"}  bgColor="white" type={"text"} placeholder="Search for items and brand" />
+        <Input onChange={(e)=>setItem(e.target.value)} borderRadius={"25px"} color="black"  bgColor="white" type={"text"} placeholder="Search for items and brand" />
       <InputRightElement width='4.5rem'>
        
-          <Search2Icon color={"black"} cursor={"pointer"} mr={-10} onClick={HandleSearch}/>
+          <Search2Icon color={"black"} cursor={"pointer"} mr={-7} onClick={HandleSearch}/>
       
       </InputRightElement>
     </InputGroup>
